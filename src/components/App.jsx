86f1f0a7014823ef0a4f02704126f4e052e2ab9e
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-01-11 23:26:50 +0200

    added global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Container } from './styles/Container/Container';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      Here will be our cool site =-)
-    </div>
+    <Container>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h2>Here will be our cool site =-)</h2>
+      </div>
+    </Container>
   );
 };
