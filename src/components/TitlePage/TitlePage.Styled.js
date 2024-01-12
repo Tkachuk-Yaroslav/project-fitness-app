4198294f4c7f8c8529e76fd69da3ad267a93846a
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-01-12 12:01:51 +0100

    add TitlePage

---
+++ src/components/TitlePage/TitlePage.Styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+export const TitleH1Styled = styled('h1')({
+  color: 'var(--white-color)',
+  fontSize: 24,
+  fontWeight: 700,
+  lineHeight: 1.17,
+});
---
+++ src/components/TitlePage/TitlePage.jsx
@@ -0,0 +1,5 @@
+import { TitleH1Styled } from './TitlePage.Styled';
+const TitlePage = ({ children }) => {
+  return <TitleH1Styled>{children}</TitleH1Styled>;
+};
+export default TitlePage;
