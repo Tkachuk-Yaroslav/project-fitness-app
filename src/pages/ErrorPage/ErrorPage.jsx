eb816089a150d29dadcf40d2e2b8e8bba0662e63
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-01-16 15:28:10 +0200

    wrapped the error page in a container

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -14,26 +14,28 @@ import {
 
 const ErrorPage = () => {
   return (
-    <Page404Container>
-      <Text404Container>
-        <LogoLink to="/">
-          <Svg>
-            <use href={`${sprite}#icon-logo-white`}></use>
-          </Svg>
-        </LogoLink>
-        <Text404>404</Text404>
-        <TextMessage404>
-          Sorry, you have reached a page that we could not find. It seems that
-          you are lost among the numbers and letters of our virtual space.
-          Perhaps this page went on vacation or decided to disappear into
-          another dimension. We apologize for this inconvenience.
-        </TextMessage404>
+    <Container>
+      <Page404Container>
+        <Text404Container>
+          <LogoLink to="/">
+            <Svg>
+              <use href={`${sprite}#icon-logo-white`}></use>
+            </Svg>
+          </LogoLink>
+          <Text404>404</Text404>
+          <TextMessage404>
+            Sorry, you have reached a page that we could not find. It seems that
+            you are lost among the numbers and letters of our virtual space.
+            Perhaps this page went on vacation or decided to disappear into
+            another dimension. We apologize for this inconvenience.
+          </TextMessage404>
 
-        <GoHomeBtn type="button">
-          <Link to="/">Go Home</Link>
-        </GoHomeBtn>
-      </Text404Container>
-    </Page404Container>
+          <GoHomeBtn type="button">
+            <Link to="/">Go Home</Link>
+          </GoHomeBtn>
+        </Text404Container>
+      </Page404Container>
+    </Container>
   );
 };
 
