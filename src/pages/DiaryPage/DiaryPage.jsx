9cc926e42761411c60f1411be45b095996752587
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-01-16 03:44:08 +0200

    added container

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,20 +1,21 @@
-
 import React from 'react';
 import DayDashboard from 'components/DayDashboard/DayDashboard';
 import DayProducts from 'components/DayProducts/DayProducts';
 import DayExercises from 'components/DayExercises/DayExercises';
 import { DiaryContainer, DiaryInfoContainer } from './DiaryPage.styled';
+import { Container } from 'components/styles/Container/Container';
 
 const DiaryPage = () => {
   return (
-    <DiaryContainer>
-      <DayDashboard />
-      <DiaryInfoContainer>
-        <DayProducts />
-        <DayExercises />
-      </DiaryInfoContainer>
-    </DiaryContainer>
-
+    <Container>
+      <DiaryContainer>
+        <DayDashboard />
+        <DiaryInfoContainer>
+          <DayProducts />
+          <DayExercises />
+        </DiaryInfoContainer>
+      </DiaryContainer>
+    </Container>
   );
 };
 
