67ce665c248226d05bd0ab5a073158ea37400ddf
Movash <andrushhenko97@gmail.com>
2024-01-18 20:18:28 +0200

    examples email

--- src/schemas/RegisterSchema/index.jsx
+++ src/schemas/RegisterSchema/index.jsx
@@ -4,7 +4,10 @@ const emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 export const registerSchema = yup.object().shape({
   name: yup.string().required('Required'),
-  email: yup.string().matches(emailPattern, 'Error email').required('Required'),
+  email: yup
+    .string()
+    .matches(emailPattern, 'Error email. Example: test@gmail.com')
+    .required('Required'),
   password: yup
     .string()
     .min(6, 'Must be 6 or more characters')
