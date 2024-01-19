d2864799b5a208390a8dc3a92a26f559b19a8bce
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-01-19 09:16:12 +0100

    fix

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,4 +1,4 @@
-import { Field, Formik } from 'formik';
+import { Formik } from 'formik';
 import {
   ButtonSave,
   FormStyled,
@@ -11,7 +11,7 @@ import CustomInput from './CustomInput';
 import CustomRadio from './CustomRadio';
 import { CustomLabelStyled, WrapperInput } from './CustomInput.styled';
 import { useSelector } from 'react-redux';
-import Calendar from 'components/Calendar/Calendar';
+// import Calendar from 'components/Calendar/Calendar';
 
 const onSubmit = async (values, actions) => {
   console.log('values', values);
