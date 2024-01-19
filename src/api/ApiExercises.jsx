e87d1df6613cb2f0d404938dbcd526287a83b081
Alona <mykyshka2013@gmail.com>
2024-01-19 14:12:18 +0200

    Update ApiExercises.jsx

--- src/api/ApiExercises.jsx
+++ src/api/ApiExercises.jsx
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://project-fitness-app-back.onrender.com/api/exercises/';
+axios.defaults.baseURL = 'https://project-fitness-app-back.onrender.com/api/';
 
 export const getExercisesBodyParts = async () => {
-    const {data} = await axios.get(`filters?filter=Body parts`)
+    const {data} = await axios.get(`exercises/filters?filter=Body parts`)
     console.log(data)
     return data
 };
 
 export const getExercisesMuscles = async () => {
-    const {data} = await axios.get(`filters?filter=Muscles`)
+    const {data} = await axios.get(`exercises/filters?filter=Muscles`)
     return data
 };
 
 export const getExercisesEquipment = async () => {
-    const {data} = await axios.get(`filters?filter=Equipment`)
+    const {data} = await axios.get(`exercises/filters?filter=Equipment`)
     return data
 };
 
