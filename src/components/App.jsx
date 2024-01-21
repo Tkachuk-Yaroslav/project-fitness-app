d9318f197309c6cdbbb867ec2e247fcd69695c59
Artem <a.g.sulimko@gmail.com>
2024-01-21 18:25:06 +0100

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -79,6 +79,8 @@ export const App = () => {
               </PrivateRoute>
             }
           />
+
+          {isLoading && <Loader />}
           <Route
             path="exercises"
             element={
