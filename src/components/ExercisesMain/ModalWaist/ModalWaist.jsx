ed931f5309de60c2987e33e7a193f5e4d864a668
Artem <a.g.sulimko@gmail.com>
2024-01-21 17:08:39 +0100

    s

--- src/components/ExercisesMain/ModalWaist/ModalWaist.jsx
+++ src/components/ExercisesMain/ModalWaist/ModalWaist.jsx
@@ -68,6 +68,65 @@ import { ButtonCloseModal, ModalStyles, ModalWrapp } from "./ModalWaist.styled";
 import { useParams } from "react-router-dom";
 import { getAllExercises } from "api/ApiExercises";
 
+// const ModalWaist = ({ filter, isOpen, closeModal, id }) => {
+//   const { body_parts, muscles, equipmentId } = useParams();
+//   const [allExercises, setExercises] = useState([]);
+
+//   const fetchAllExercises = async () => {
+//     try {
+//       const exercises = await getAllExercises(
+//         filter,
+//         body_parts || muscles || equipmentId
+//       );
+//       setExercises(exercises);
+//     } catch (err) {
+//       console.log(err.message);
+//     }
+//   };
+
+//   console.log("paramsbody_parts=", body_parts);
+//   console.log("paramsequipmentId=", equipmentId);
+//   console.log("paramsmuscles=", muscles);
+
+//   useEffect(() => {
+//     fetchAllExercises();
+//     // eslint-disable-next-line
+//   }, []);
+
+//   console.log("Exercises=", allExercises);
+//   console.log("id=", id);
+
+//   return (
+//     <Modal style={ModalStyles} isOpen={isOpen} onRequestClose={closeModal}>
+//       <ModalWrapp>
+//         <ButtonCloseModal onClick={closeModal}>X</ButtonCloseModal>
+//         <h2> Название модального окна</h2>
+//         <p>Текст модального окна</p>
+
+//         {allExercises.map(
+//           (exercise, index) =>
+//             exercise._id === id && (
+//               <div className="ExerciseItemt" key={index}>
+//                 <p>ID={exercise._id}</p>
+//                 <img width={100} src={exercise.gifUrl} alt={exercise.name} />
+//                 <p>Name:{exercise.name}</p>
+//                 <p>BurnedCalories:{exercise.burnedCalories}</p>
+//                 <p>Equipment:{exercise.equipment}</p>
+//                 <p>Target:{exercise.target}</p>
+//                 <p>BodyPart:{exercise.bodyPart}</p>
+//                 <p>Time:{exercise.time}</p>
+//               </div>
+//             )
+//         )}
+//       </ModalWrapp>
+//     </Modal>
+//   );
+// };
+
+// export default ModalWaist;
+
+// ... (previous imports and component setup)
+
 const ModalWaist = ({ filter, isOpen, closeModal, id }) => {
   const { body_parts, muscles, equipmentId } = useParams();
   const [allExercises, setExercises] = useState([]);
@@ -105,10 +164,14 @@ const ModalWaist = ({ filter, isOpen, closeModal, id }) => {
 
         {allExercises.map(
           (exercise, index) =>
-            exercise._id === id && ( // Corrected the arrow function syntax here
+            exercise._id === id && (
               <div className="ExerciseItemt" key={index}>
                 <p>ID={exercise._id}</p>
-                <img width={100} src={exercise.gifUrl} alt={exercise.name} />
+                {exercise.gifUrl ? (
+                  <img width={100} src={exercise.gifUrl} alt={exercise.name} />
+                ) : (
+                  <p>No Image Available</p>
+                )}
                 <p>Name:{exercise.name}</p>
                 <p>BurnedCalories:{exercise.burnedCalories}</p>
                 <p>Equipment:{exercise.equipment}</p>
