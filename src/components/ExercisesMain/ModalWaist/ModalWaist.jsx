0cdd006bab189eef4a7e0769a289454178b1fc98
Andrii Snishchuk <127045868+andsnk@users.noreply.github.com>
2024-01-23 03:35:48 +0200

    fix modal

--- src/components/ExercisesMain/ModalWaist/ModalWaist.jsx
+++ src/components/ExercisesMain/ModalWaist/ModalWaist.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
+import 'overlayscrollbars/overlayscrollbars.css';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import {
   ButtonCloseModal,
   ExerciseContent,
   Gif,
   ModalStyles,
   ModalWrapp,
+  Svg,
+  ContentWrap,AddBtn
 } from './ModalWaist.styled';
 import { useParams } from 'react-router-dom';
 import { getAllExercises } from 'api/ApiExercises';
 import ModalWaistList from './ModalWaistList';
 import sprite from '../../../images/sprite.svg';
 import CountdownTimer from './CountdownTimer';
+import './stylesModal.css';
 
 const ModalWaist = ({ filter, isOpen, closeModal, id }) => {
   const { body_parts, muscles, equipmentId } = useParams();
@@ -38,30 +43,43 @@ const ModalWaist = ({ filter, isOpen, closeModal, id }) => {
     <Modal style={ModalStyles} isOpen={isOpen} onRequestClose={closeModal}>
       <ModalWrapp>
         <ButtonCloseModal onClick={closeModal}>
-          <svg>
-            <use href={`${sprite}#icon-x`} />
-          </svg>
+          <Svg>
+            <use href={`${sprite}#icon-x-modal`} />
+          </Svg>
         </ButtonCloseModal>
 
         {allExercises.map(
           (exercise, index) =>
             exercise._id === id && (
               <div className="ExerciseItemt" key={index}>
-                <ExerciseContent>
+                {/* <ExerciseContent>
                   <Gif src={exercise.gifUrl} alt={exercise.name} />
                   <ModalWaistList
                     name={exercise.name}
                     bodypart={exercise.bodyPart}
                     target={exercise.target}
+                    equipment={exercise.e
+                      quipment}
+                  /> */}
+
+                <ExerciseContent>
+                  <Gif src={exercise.gifUrl} alt={exercise.name} />
+                 <ContentWrap>
+                 <ModalWaistList
+                    name={exercise.name}
+                    bodypart={exercise.bodyPart}
+                    target={exercise.target}
                     equipment={exercise.equipment}
                   />
-                </ExerciseContent>
                 <CountdownTimer
                   customKey={exercise._id}
                   timer={3}
                   handleTime={() => {}}
                   dynamicBurnCal={0}
                 />
+                 </ContentWrap>
+                <AddBtn>Add to diary</AddBtn>
+                </ExerciseContent>
               </div>
             )
         )}
