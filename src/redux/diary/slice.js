031e633b09691ab6095fc307b3a022cd536a0451
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2024-01-23 18:53:42 +0200

    diary_redax

---
+++ src/redux/diary/slice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchBodyParts, fetchEquipment, fetchMuscles, setExerciseThunk } from './thunks';
+
+const initialState = {
+  exercises: [],
+  products: [],
+  isLoading: false,
+  error: null,
+};
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+export const diarySlice = createSlice({
+  name: 'diary',
+  initialState,
+
+  extraReducers: builder => builder
+  .addCase(setExerciseThunk.fulfilled, (state, {payload})=>{
+    state.exercises = [...state.exercises, payload.info]
+  })
+
+});
+
+export const diaryReducer = diarySlice.reducer;
