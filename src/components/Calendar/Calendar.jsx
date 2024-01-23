b7d14498b3a632df156bece57672accf3f24688f
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-01-23 12:16:24 +0100

    fix style calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -7,40 +7,40 @@ import { useState, useEffect } from 'react';
 import { useField } from 'formik';
 
 const Calendar = ({ name }) => {
-	const [selectedDate, setSelectedDate] = useState(null);
-	const [field, , { setValue }] = useField(name);
-	useEffect(() => {
-		handleDateChange(new Date(field.value));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [field, , { setValue }] = useField(name);
+  useEffect(() => {
+    handleDateChange(new Date(field.value));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	const handleDateChange = async (date) => {
-		setSelectedDate(date);
-		setValue(date);
-		console.log(field);
-	};
-	return (
-		<>
-			<div style={{ width: '187px', position: 'relative' }}>
-				<IconSvg width='18' height='18'>
-					<use href={`${sprite}#icon-calendar-white`}></use>
-				</IconSvg>
+  const handleDateChange = async date => {
+    setSelectedDate(date);
+    setValue(date);
+    console.log(field);
+  };
+  return (
+    <>
+      <div style={{ width: 150, position: 'relative', paddingTop: 22 }}>
+        <IconSvg width="18" height="18">
+          <use href={`${sprite}#icon-calendar-white`}></use>
+        </IconSvg>
 
-				<DatePicker
-					{...field}
-					selected={selectedDate}
-					onChange={handleDateChange}
-					customInput={<InputField name={name} style={{ cursor: 'pointer' }} />}
-					dateFormat={'dd/MM/yyyy'}
-					calendarStartDay={1}
-					formatWeekDay={(day) => day.substring(0, 1)}
-					placeholderText={selectedDate === null ? 'Birthday' : null}
-				/>
+        <DatePicker
+          {...field}
+          selected={selectedDate}
+          onChange={handleDateChange}
+          customInput={<InputField name={name} style={{ cursor: 'pointer' }} />}
+          dateFormat={'dd/MM/yyyy'}
+          calendarStartDay={1}
+          formatWeekDay={day => day.substring(0, 1)}
+          placeholderText={selectedDate === null ? 'Birthday' : null}
+        />
 
-				<CalendarGlobalStyles />
-			</div>
-		</>
-	);
+        <CalendarGlobalStyles />
+      </div>
+    </>
+  );
 };
 
 export default Calendar;
