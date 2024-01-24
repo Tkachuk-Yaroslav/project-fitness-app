c951a3a5f8324eb165f003f0287a5786c9176667
Artem <a.g.sulimko@gmail.com>
2024-01-24 14:36:11 +0100

    s

--- src/components/ExercisesMain/ModalWaist/ModalWaist.jsx
+++ src/components/ExercisesMain/ModalWaist/ModalWaist.jsx
@@ -1,104 +1,117 @@
-import React, { useEffect, useState } from 'react';
-import Modal from 'react-modal';
-import 'overlayscrollbars/overlayscrollbars.css';
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
+import "overlayscrollbars/overlayscrollbars.css";
 import {
-	ButtonCloseModal,
-	ExerciseContent,
-	Gif,
-	ModalStyles,
-	ModalWrapp,
-	Svg,
-	ContentWrap,
-	AddBtn,
-} from './ModalWaist.styled';
-import { useParams } from 'react-router-dom';
-import { getAllExercises } from 'api/ApiExercises';
-import ModalWaistList from './ModalWaistList';
-import sprite from '../../../images/sprite.svg';
-import CountdownTimer from './CountdownTimer';
-import './stylesModal.css';
-import { useDispatch } from 'react-redux';
-import { setExerciseThunk } from '../../../redux/diary/thunks';
-
+  ButtonCloseModal,
+  ExerciseContent,
+  Gif,
+  ModalStyles,
+  ModalWrapp,
+  Svg,
+  ContentWrap,
+  AddBtn,
+} from "./ModalWaist.styled";
+import { useParams } from "react-router-dom";
+import { getAllExercises } from "api/ApiExercises";
+import ModalWaistList from "./ModalWaistList";
+import sprite from "../../../images/sprite.svg";
+import CountdownTimer from "./CountdownTimer";
+import "./stylesModal.css";
+import { useDispatch } from "react-redux";
+import { setExerciseThunk } from "../../../redux/diary/thunks";
+import toast from "react-hot-toast";
 const ModalWaist = ({ filter, isOpen, closeModal, id }) => {
-	const dispatch = useDispatch();
-	const { body_parts, muscles, equipmentId } = useParams();
-	const [allExercises, setExercises] = useState([]);
-	const [selectedExercise, setSelectedExercise] = useState(null);
-	const [remaningTime, setRemaningTime] = useState();
-	const [burnedCalories, setBurnedCalories] = useState();
-	const fetchAllExercises = async () => {
-		try {
-			const exercises = await getAllExercises(filter, body_parts || muscles || equipmentId);
-			setExercises(exercises);
-		} catch (err) {
-			console.log(err.message);
-		}
-	};
+  const dispatch = useDispatch();
+  const { body_parts, muscles, equipmentId } = useParams();
+  const [allExercises, setExercises] = useState([]);
+  const [selectedExercise, setSelectedExercise] = useState(null);
+  const [remaningTime, setRemaningTime] = useState();
+  const [burnedCalories, setBurnedCalories] = useState();
+  const fetchAllExercises = async () => {
+    try {
+      const exercises = await getAllExercises(
+        filter,
+        body_parts || muscles || equipmentId
+      );
+      setExercises(exercises);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
-	useEffect(() => {
-		fetchAllExercises();
-		// eslint-disable-next-line
-	}, []);
+  useEffect(() => {
+    fetchAllExercises();
+    // eslint-disable-next-line
+  }, []);
 
-	const handleSubmit = () => {
-		const currentDate = `${String(new Date().getDate()).padStart(2, '0')}/${String(
-  new Date().getMonth() + 1
-).padStart(2, '0')}/${new Date().getFullYear()}`;
-		const data = {
-			exerciseId: selectedExercise._id,
-			date: currentDate,
-			time: 180 - remaningTime,
-			calories: burnedCalories,
-		};
-    console.log(data)
-		dispatch(setExerciseThunk(data));
-		// відправка даних
-	};
+  const handleSubmit = () => {
+    const currentDate = `${String(new Date().getDate()).padStart(
+      2,
+      "0"
+    )}/${String(new Date().getMonth() + 1).padStart(
+      2,
+      "0"
+    )}/${new Date().getFullYear()}`;
+    const data = {
+      exerciseId: selectedExercise._id,
+      date: currentDate,
+      time: 180 - remaningTime,
+      calories: burnedCalories,
+    };
+    console.log(data);
+    dispatch(setExerciseThunk(data));
+    // відправка даних
+    remaningTime
+      ? setExerciseThunk()
+      : toast.error("Exercise not completed", {
+          duration: 3000,
+          position: "top-center",
+        });
+  };
 
-	return (
-		<Modal style={ModalStyles} isOpen={isOpen} onRequestClose={closeModal}>
-			<ModalWrapp>
-				<ButtonCloseModal onClick={closeModal}>
-					<Svg>
-						<use href={`${sprite}#icon-x-modal`} />
-					</Svg>
-				</ButtonCloseModal>
+  return (
+    <Modal style={ModalStyles} isOpen={isOpen} onRequestClose={closeModal}>
+      <ModalWrapp>
+        <ButtonCloseModal onClick={closeModal}>
+          <Svg>
+            <use href={`${sprite}#icon-x-modal`} />
+          </Svg>
+        </ButtonCloseModal>
 
-				{allExercises.map(
-					(exercise, index) =>
-						exercise._id === id && (
-							<div
-								className='ExerciseItemt'
-								key={index}
-								onClick={() => setSelectedExercise(exercise)}
-							>
-								<ExerciseContent>
-									<Gif src={exercise.gifUrl} alt={exercise.name} />
-									<ContentWrap>
-										<ModalWaistList
-											name={exercise.name}
-											bodypart={exercise.bodyPart}
-											target={exercise.target}
-											equipment={exercise.equipment}
-										/>
-										<CountdownTimer
-											timer={`${exercise.time}`}
-											handleTime={(value) => setRemaningTime(value)}
-											handleCalories={setBurnedCalories}
-											dynamicBurnCal={`${exercise.burnedCalories}`}
-										/>
-									</ContentWrap>
-									<AddBtn type='button' onClick={handleSubmit}>
-										Add to diary
-									</AddBtn>
-								</ExerciseContent>
-							</div>
-						)
-				)}
-			</ModalWrapp>
-		</Modal>
-	);
+        {allExercises.map(
+          (exercise, index) =>
+            exercise._id === id && (
+              <div
+                className="ExerciseItemt"
+                key={index}
+                onClick={() => setSelectedExercise(exercise)}
+              >
+                <ExerciseContent>
+                  <Gif src={exercise.gifUrl} alt={exercise.name} />
+                  <ContentWrap>
+                    <ModalWaistList
+                      name={exercise.name}
+                      bodypart={exercise.bodyPart}
+                      target={exercise.target}
+                      equipment={exercise.equipment}
+                    />
+                    <CountdownTimer
+                      timer={`${exercise.time}`}
+                      handleTime={(value) => setRemaningTime(value)}
+                      handleCalories={setBurnedCalories}
+                      dynamicBurnCal={`${exercise.burnedCalories}`}
+                    />
+                  </ContentWrap>
+                  <AddBtn type="button" onClick={handleSubmit}>
+                    Add to diary
+                  </AddBtn>
+                </ExerciseContent>
+              </div>
+            )
+        )}
+      </ModalWrapp>
+    </Modal>
+  );
 };
 
 export default ModalWaist;
--- src/redux/diary/thunks.js
+++ src/redux/diary/thunks.js
@@ -1,14 +1,19 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { setExercise } from "api/dairy";
-
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { setExercise } from 'api/dairy';
+import toast from 'react-hot-toast';
 export const setExerciseThunk = createAsyncThunk(
-    'exercises/setExercise',
-    async (data, { rejectWithValue }) => {
-      try {
-        const result = await setExercise(data)
-        return result
-      } catch (e) {
-        return rejectWithValue(e.message);
-      }
+  'exercises/setExercise',
+  async (data, { rejectWithValue }) => {
+    try {
+      const result = await setExercise(data);
+      return result;
+    } catch (e) {
+      return rejectWithValue(
+        toast.error('Failed to add exercise to dairy', {
+          duration: 3000,
+          position: 'top-center',
+        })
+      );
     }
-  )
+  }
+);
