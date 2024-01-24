fe7797a9d8084894744251f678b1ce006f6eba26
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2024-01-24 05:46:47 +0200

    fixed error

--- src/components/ProductsListItem/ProductsListItem.jsx
+++ src/components/ProductsListItem/ProductsListItem.jsx
@@ -35,65 +35,64 @@ const ProductsListItem = ({
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModalToggle = () => {
-   setIsModalOpen(!isModalOpen);
+    setIsModalOpen(!isModalOpen);
   };
   return (
-    <ProductsCard>
-      <ProductsCardStatus>
-        <ProductsCardDiet>
-          <ProductsCardDietText>{category}</ProductsCardDietText>
-        </ProductsCardDiet>
-        <ProductsCardStatusCount>
-          <StyleSheetManager>
-            <ProductsCardStatusCountTrue $recommended={firstValue}>
-              {firstValue ? 'Recommended' : 'Not recommended'}
-            </ProductsCardStatusCountTrue>
-          </StyleSheetManager>
-          <ProductsCardStatusAdd
-            onClick={openModalToggle}
-            type="button"
-          >
-            Add
-            <SvgExercise>
-              <use href={`${sprite}#icon-arrow-right`}></use>
-            </SvgExercise>
-          </ProductsCardStatusAdd>
-        </ProductsCardStatusCount>
-      </ProductsCardStatus>
-      <IconTitleDiv>
-        <SpanIconRun>
-          <SvgIconRun width={24} height={24}>
-            <use href={`${sprite}#icon-run-exercises`}></use>
-          </SvgIconRun>
-        </SpanIconRun>
-        <ProductsCardTitle>{title ? title : 'title'}</ProductsCardTitle>
-      </IconTitleDiv>
+    <>
+      <ProductsCard>
+        <ProductsCardStatus>
+          <ProductsCardDiet>
+            <ProductsCardDietText>{category}</ProductsCardDietText>
+          </ProductsCardDiet>
+          <ProductsCardStatusCount>
+            <StyleSheetManager>
+              <ProductsCardStatusCountTrue $recommended={firstValue}>
+                {firstValue ? 'Recommended' : 'Not recommended'}
+              </ProductsCardStatusCountTrue>
+            </StyleSheetManager>
+            <ProductsCardStatusAdd onClick={openModalToggle} type="button">
+              Add
+              <SvgExercise>
+                <use href={`${sprite}#icon-arrow-right`}></use>
+              </SvgExercise>
+            </ProductsCardStatusAdd>
+          </ProductsCardStatusCount>
+        </ProductsCardStatus>
+        <IconTitleDiv>
+          <SpanIconRun>
+            <SvgIconRun width={24} height={24}>
+              <use href={`${sprite}#icon-run-exercises`}></use>
+            </SvgIconRun>
+          </SpanIconRun>
+          <ProductsCardTitle>{title ? title : 'title'}</ProductsCardTitle>
+        </IconTitleDiv>
 
-      <ProductsCardInfoList>
-        <ProductsCardInfoItem>
-          <ProductsCardInfoValue>
-            Calories:<Value>{calories}</Value>
-          </ProductsCardInfoValue>
-        </ProductsCardInfoItem>
-        <ProductsCardInfoItem>
-          <ProductsCardInfoValue>
-            Category:<Value> {category}</Value>
-          </ProductsCardInfoValue>
-        </ProductsCardInfoItem>
-        <ProductsCardInfoItem>
-          <ProductsCardInfoValue>
-            Weight:<Value>{weight}</Value>
-          </ProductsCardInfoValue>
-        </ProductsCardInfoItem>
-      </ProductsCardInfoList>
-    </ProductsCard>
-    <ModalProducts
+        <ProductsCardInfoList>
+          <ProductsCardInfoItem>
+            <ProductsCardInfoValue>
+              Calories:<Value>{calories}</Value>
+            </ProductsCardInfoValue>
+          </ProductsCardInfoItem>
+          <ProductsCardInfoItem>
+            <ProductsCardInfoValue>
+              Category:<Value> {category}</Value>
+            </ProductsCardInfoValue>
+          </ProductsCardInfoItem>
+          <ProductsCardInfoItem>
+            <ProductsCardInfoValue>
+              Weight:<Value>{weight}</Value>
+            </ProductsCardInfoValue>
+          </ProductsCardInfoItem>
+        </ProductsCardInfoList>
+      </ProductsCard>
+      <ModalProducts
         id={id}
         title={title}
         calories={calories}
         isOpen={isModalOpen}
         onClose={openModalToggle}
       />
+    </>
   );
 };
 
